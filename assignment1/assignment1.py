00a268e813436b28a8b8e5f0e095862b2c119efd
Tianxin He <19434413@life.hkbu.edu.hk>
2020-01-30 21:23:24 +0800

    Assginment 1 Connect redis; Return how many times of a keyword/query is typed; Increment the counter of the keyword when it is typed;

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
-import redis
 
+import redis
 
 # fill in the following.
 HOST = "redis-11363.c1.asia-northeast1-1.gce.cloud.redislabs.com"
@@ -20,7 +20,13 @@ while True:
 
    
     # Add your code here
+    value = redis1.get(msg)
+    if value == None:
+        redis1.set(msg,1)
+        get = redis1.get(msg)
+    else: 
+        value_int=int(value)
+        vt = value_int + 1
+        get = redis1.getset(msg, vt)
 
-
-
-    print('for X times')
+    print('for '+ get.decode() + ' times' ) 
