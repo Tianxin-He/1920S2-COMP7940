98e185b755b05ced9b3408f6e18563a2d1f94ed1
Tianxin He <19434413@life.hkbu.edu.hk>
2020-01-30 23:04:32 +0800

    Change the redis server fix a bug

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -4,9 +4,13 @@ from __future__ import unicode_literals
 import redis
 
 # fill in the following.
-HOST = "redis-11363.c1.asia-northeast1-1.gce.cloud.redislabs.com"
-PWD = "1nOA0St0I7p9pQqu8HkQ18XqDfnoPeoL"
-PORT = "11363" 
+# HOST = "redis-11363.c1.asia-northeast1-1.gce.cloud.redislabs.com"
+# PWD = "1nOA0St0I7p9pQqu8HkQ18XqDfnoPeoL"
+# PORT = "11363" 
+
+HOST = "redis-15288.c16.us-east-1-3.ec2.cloud.redislabs.com"
+PWD = "TE7ntZzxOTUByAsEbINMBAKVtBq8oROi"
+PORT = "15288" 
 
 redis1 = redis.Redis(host = HOST, password = PWD, port = PORT)
 
@@ -22,11 +26,11 @@ while True:
     # Add your code here
     value = redis1.get(msg)
     if value == None:
-        redis1.set(msg,1)
-        get = redis1.get(msg)
+        print('for 1 times' )
+        redis1.set(msg,2)
     else: 
         value_int=int(value)
         vt = value_int + 1
         get = redis1.getset(msg, vt)
-
-    print('for '+ get.decode() + ' times' ) 
+        print('for '+ get.decode() + ' times' )
+     
